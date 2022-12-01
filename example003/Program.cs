0c6cf8edcade9cd254ecc23ea2ff115b40a960c3
george <georgij5ptb@gmail.com>
2022-12-01 11:24:20 +0300

    Добавили рандом чисел

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,45); // 0 1 2 3 ... 44
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,45);
+
+Console.WriteLine(numberB);
+
 int Sum = numberA+numberB;
 
 Console.WriteLine(Sum);
