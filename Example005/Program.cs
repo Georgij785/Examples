c079ba76ca2a201e736f4a7e1ef34ef58a0d3c78
george <georgij5ptb@gmail.com>
2022-12-01 11:34:08 +0300

    Добавили приветствие юзера , если это Maria , то особенное приветствие

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What's your mame?");
+string userName= Console.ReadLine();
+if (userName == "Maria")
+{
+    Console.WriteLine("Привет, Машенька");
+}
+else
+{
+    Console.WriteLine("Привет, " + userName);
+}
