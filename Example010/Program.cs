8225ede217359fe37d635594e469d49124dee7de
george <georgij5ptb@gmail.com>
2022-12-02 13:41:42 +0300

    цикл определения индекса

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 4, 6, 7, 86, 56, 5, 467, 3, 36578 };
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("искомое число находится в массиве под индексом " + index);
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+}
